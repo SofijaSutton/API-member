35208fdabc93aa1556354f23078ade7766216c63
SofijaSutton <srlsutton@gmail.com>
2021-05-02 16:27:34 -0400

    Feat: attempts to load static files, still bugs

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+//const db = require('./db');
+const routes = require("./Routes/api");
+
 const app = express();
-const bodyParser = require('body-parser');
-const db = require('./db/members');
-const routes = require('./Routes/api');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //home page
-app.use('/', routes);
+app.use("/", routes);
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, "..", "public")));
+//app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke! Route does not exist.');
+	console.error(err.stack);
+	res.status(500).send("Something broke! Route does not exist.");
+});
+
+app.use("*", function (req, res, next) {
+	res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 const init = async () => {
-    try {
-    //  await db.sync();
-      app.listen(3000, () => {
-        console.log('Server is listening on port 3000!');
-      })
-    } catch (err) {
-      console.error(err);
-    }
-}
+	try {
+		//  await db.sync();
+		app.listen(3000, () => {
+			console.log("Server is listening on port 3000!");
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
 
 init();
 
